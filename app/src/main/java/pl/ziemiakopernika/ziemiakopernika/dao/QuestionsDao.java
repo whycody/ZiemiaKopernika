ba97cb5f676f9764b1ef8dfabc89d4d01e253b4e
Oktawian Kausz <play26400@gmail.com>
2019-03-21 19:01:36 +0100

    Created DAO classes

---
+++ app/src/main/java/pl/ziemiakopernika/ziemiakopernika/dao/QuestionsDao.java
@@ -0,0 +1,13 @@
+package pl.ziemiakopernika.ziemiakopernika.dao;
+
+import java.util.ArrayList;
+
+import pl.ziemiakopernika.ziemiakopernika.model.Answer;
+import pl.ziemiakopernika.ziemiakopernika.model.Question;
+
+public interface QuestionsDao {
+
+    ArrayList<Question> getRandomQustions(int number);
+
+    ArrayList<Answer> getRandomAnswers(int number);
+}
--- app/src/main/java/pl/ziemiakopernika/ziemiakopernika/main/MainPresenterImpl.java
+++ app/src/main/java/pl/ziemiakopernika/ziemiakopernika/main/MainPresenterImpl.java
@@ -8,6 +8,8 @@ import android.content.SharedPreferences;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import pl.ziemiakopernika.ziemiakopernika.dao.QuestionsDao;
+import pl.ziemiakopernika.ziemiakopernika.dao.QuestionsDaoImpl;
 import pl.ziemiakopernika.ziemiakopernika.redinfo.RedInfoActivity;
 import pl.ziemiakopernika.ziemiakopernika.model.Answer;
 import pl.ziemiakopernika.ziemiakopernika.model.Question;
@@ -18,8 +20,9 @@ public class MainPresenterImpl implements MainPresenter{
     private Activity activity;
     private MainActivityView activityView;
     private SharedPreferences sharedPreferences;
-    private static String MUTE_ENABLED = "mute_enabled";
+    private QuestionsDao questionsDao;
 
+    private static String MUTE_ENABLED = "mute_enabled";
     private int secondsPerQuestion = 20;
     private int numberOfQuestions = 5;
 
@@ -27,6 +30,7 @@ public class MainPresenterImpl implements MainPresenter{
         this.activity = activity;
         this.activityView = activityView;
         sharedPreferences = activity.getPreferences(Context.MODE_PRIVATE);
+        questionsDao = new QuestionsDaoImpl();
     }
 
     @Override
@@ -47,38 +51,8 @@ public class MainPresenterImpl implements MainPresenter{
     }
 
     private SetOfQuestions getSetOfQuestions(){
-        return new SetOfQuestions(numberOfQuestions,secondsPerQuestion, getQuestions(), getAnswers());
-    }
-
-    private ArrayList<Question> getQuestions(){
-        ArrayList<Question> questions = new ArrayList<>();
-        questions.add(new Question("Który księżyc jako jedyny w Układzie Słonecznym posiada gęstą atmosferę?",
-                "Tytan", "Księżyc", "Io", "Europa"));
-        questions.add(new Question("Jądro której galaktyki znajduje się najbliżej Układu Słonecznego?",
-                "Karzeł Wielkiego Psa", "Karzeł Strzelca",
-                "Wielki Obłok Magellana", "Droga Mleczna"));
-        questions.add(new Question("Jaką średnicę ma Mars?", "6794 km",
-                "6894 km", "6994 km", "6694 km"));
-        questions.add(new Question("Fobos jest księżycem której planety?",
-                "Marsa", "Uranu", "Saturna", "Jowisza"));
-        questions.add(new Question("Co było pierwszą udaną misją planetarną NASA?",
-                "Zbliżenie się do Wenus", "Zbliżenie się do Słońca",
-                "Zbliżenie się do Plutona", "Zbliżenie się do Marsa"));
-        return questions;
-    }
-
-    private ArrayList<Answer> getAnswers(){
-        ArrayList<Answer> answers = new ArrayList<>();
-        for(int i =1; i<=numberOfQuestions; i++){
-            ArrayList<Integer> integers = new ArrayList<>();
-            integers.add(1);
-            integers.add(2);
-            integers.add(3);
-            integers.add(4);
-            Collections.reverse(integers);
-            answers.add(new Answer(0, integers));
-        }
-        return answers;
+        return new SetOfQuestions(numberOfQuestions,secondsPerQuestion,
+                questionsDao.getRandomQustions(5), questionsDao.getRandomAnswers(5));
     }
 
     @Override

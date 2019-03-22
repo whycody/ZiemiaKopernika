1a64d7e17c9d268e53a030efe4b2d8b23af56fd2
Oktawian Kausz <play26400@gmail.com>
2019-03-22 22:02:41 +0100

    Improved design of QuestionActivity

---
+++ app/src/main/java/pl/ziemiakopernika/ziemiakopernika/ZeroTypeAnswersFragment.java
@@ -0,0 +1,17 @@
+package pl.ziemiakopernika.ziemiakopernika;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ZeroTypeAnswersFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_zero_type_answers, container, false);
+    }
+
+}
--- app/src/main/java/pl/ziemiakopernika/ziemiakopernika/question/QuestionActivity.java
+++ app/src/main/java/pl/ziemiakopernika/ziemiakopernika/question/QuestionActivity.java
@@ -1,9 +1,12 @@
 package pl.ziemiakopernika.ziemiakopernika.question;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import pl.ziemiakopernika.ziemiakopernika.R;
+import pl.ziemiakopernika.ziemiakopernika.ZeroTypeAnswersFragment;
 
 public class QuestionActivity extends AppCompatActivity {
 
@@ -11,5 +14,20 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        applyFragment();
+    }
+
+    private void applyFragment(){
+        Fragment fragment = new ZeroTypeAnswersFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }

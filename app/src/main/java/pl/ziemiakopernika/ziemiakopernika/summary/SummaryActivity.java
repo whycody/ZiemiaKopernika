a9bd485c4ff13690ee9a45d587d3d4caebb739ff
Oktawian Kausz <play26400@gmail.com>
2019-04-08 19:28:08 +0200

    Added application icon

---
+++ app/src/main/java/pl/ziemiakopernika/ziemiakopernika/summary/SummaryActivity.java
@@ -0,0 +1,15 @@
+package pl.ziemiakopernika.ziemiakopernika.summary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pl.ziemiakopernika.ziemiakopernika.R;
+
+public class SummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+}
